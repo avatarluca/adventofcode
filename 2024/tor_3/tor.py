d53baa7c0be4015662b1e39eadbb450a845561d0
marceluc <marceluc@students.zhaw.ch>
2024-12-05 08:08:32 +0100

    2024 folder

---
+++ 2024/tor_3/tor.py
@@ -0,0 +1,47 @@
+def load_input():
+    with open("c:/Users/lucam/OneDrive/Desktop/advent_of_code/adventofcode/2024/tor_3/input.txt", "r") as f:
+        return f.read().strip()
+
+def part1(input):
+    import re
+    pattern = r"mul\((\d+),(\d+)\)"
+    matches = re.findall(pattern, input)
+    sum = 0
+    for match in matches:
+        sum += int(match[0]) * int(match[1])
+
+    return sum
+
+
+def part2(input):
+    import re
+    
+    mul_pattern = r"mul\((\d+),(\d+)\)"
+    total_sum = 0
+
+    enabled = True
+
+    for i in range(len(input)): # linear durch input weil wenn dont dann alle nachfolgenden mul ignoriert werden
+        # chamer no besser mache idem i nöd nur immer +1 macht sondern halt denn i je nachdem wie lang das wort isch verschiebt
+        if input[i:].startswith("don't()"):
+            enabled = False
+        elif input[i:].startswith("do()"):
+            enabled = True
+        
+        if enabled:
+            mul_match = re.match(mul_pattern, input[i:])
+            if mul_match: # nur wenn mul am anfang vo dem substring ist (sonst könnt dont()do()dont()mul() nicht funktionieren)
+                total_sum += int(mul_match.group(1)) * int(mul_match.group(2))
+    
+    return total_sum
+
+
+def main():
+    input = load_input()
+
+    print(part1(input))
+    print(part2(input))
+
+
+if __name__ == "__main__":
+    main()
---
+++ 2024/tor_4/tor.py
@@ -0,0 +1,71 @@
+def load_input():
+    with open("c:/Users/lucam/OneDrive/Desktop/advent_of_code/adventofcode/2024/tor_4/input.txt", "r") as f:
+        return f.read().strip()
+
+def part1(input):
+    count = 0
+
+    # horizontal
+    for line in input.split("\n"):
+        for i in range(len(line) - 3):
+            if line[i:i+3] == "XMAS":
+                count += 1
+            elif line[i:i+3] == "SAMX":
+                count += 1
+
+    # vertikale   
+    for i in range(len(input.split("\n")) - 3):
+        for j in range(len(input.split("\n")[i])):
+            if input.split("\n")[i][j] == "X" and input.split("\n")[i+1][j] == "M" and input.split("\n")[i+2][j] == "A" and input.split("\n")[i+3][j] == "S":
+                count += 1
+            elif input.split("\n")[i][j] == "S" and input.split("\n")[i+1][j] == "A" and input.split("\n")[i+2][j] == "M" and input.split("\n")[i+3][j] == "X":
+                count += 1
+    
+    # diagonal links rechts
+    for i in range(len(input.split("\n")) - 3):
+        for j in range(len(input.split("\n")[i]) - 3):
+            if input.split("\n")[i][j] == "X" and input.split("\n")[i+1][j+1] == "M" and input.split("\n")[i+2][j+2] == "A" and input.split("\n")[i+3][j+3] == "S":
+                count += 1
+            elif input.split("\n")[i][j] == "S" and input.split("\n")[i+1][j+1] == "A" and input.split("\n")[i+2][j+2] == "M" and input.split("\n")[i+3][j+3] == "X":
+                count += 1
+    
+    # diagonal rechts links
+    for i in range(len(input.split("\n")) - 3):
+        for j in range(3, len(input.split("\n")[i])): # fangt bi 3 a, will chliner nöd möglich
+            if input.split("\n")[i][j] == "X" and input.split("\n")[i+1][j-1] == "M" and input.split("\n")[i+2][j-2] == "A" and input.split("\n")[i+3][j-3] == "S":
+                count += 1
+            elif input.split("\n")[i][j] == "S" and input.split("\n")[i+1][j-1] == "A" and input.split("\n")[i+2][j-2] == "M" and input.split("\n")[i+3][j-3] == "X":
+                count += 1
+
+    # TODO: Optimierig wer wenn mehr wenn bspw. XMAS gfunde wird, dass er denn die loops jewils au um 4 verschiebt
+
+    return count
+
+
+def part2(input):
+    count = 0
+    for i in range(len(input.split("\n")) - 2): 
+        for j in range(len(input.split("\n")[i]) - 2): 
+            if input.split("\n")[i][j] == "M" and input.split("\n")[i+1][j+1] == "A" and input.split("\n")[i+2][j+2] == "S":
+                if input.split("\n")[i][j+2] == "M" and input.split("\n")[i+1][j+1] == "A" and input.split("\n")[i+2][j] == "S":
+                    count += 1
+                elif input.split("\n")[i][j+2] == "S" and input.split("\n")[i+1][j+1] == "A" and input.split("\n")[i+2][j] == "M":
+                    count += 1
+            elif input.split("\n")[i][j] == "S" and input.split("\n")[i+1][j+1] == "A" and input.split("\n")[i+2][j+2] == "M":
+                if input.split("\n")[i][j+2] == "M" and input.split("\n")[i+1][j+1] == "A" and input.split("\n")[i+2][j] == "S":
+                    count += 1
+                elif input.split("\n")[i][j+2] == "S" and input.split("\n")[i+1][j+1] == "A" and input.split("\n")[i+2][j] == "M":
+                    count += 1
+    
+    return count
+
+
+def main():
+    input = load_input()
+
+    print(part1(input))
+    print(part2(input))
+
+
+if __name__ == "__main__":
+    main()
